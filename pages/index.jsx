c45a7a97b14edec6e4822a45b413d55d48eecb16
amin <amin.m.intichev@gmail.com>
2021-08-23 20:19:29 +0200

    front end quiz example + styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,26 @@
-import Head from 'next/head'
-import Layout from "../components/layout";
-
-export default function Home() {
+export default function Index() {
     return (
         <>
-            {/*<Head>*/}
-            {/*    <title>Trivalia</title>*/}
-            {/*    <meta name="description" content="Welcome to Trivalia. The most anticipated quiz show of many things."/>*/}
-            {/*    <link rel="icon" href="/favicon.ico"/>*/}
-            {/*</Head>*/}
-            {/*<div className='md:w-4/6 md:container mx-auto flex flex-col justify-center items-center'>*/}
-            {/*    <header>*/}
-            {/*        <h1 className='text-3xl md:text-4xl font-bold'>Trivalia</h1>*/}
-            {/*    </header>*/}
-            {/*    <main>*/}
-            {/*        Click start to begin trivia.*/}
-            {/*    </main>*/}
-            {/*</div>*/}
+            <h1 className='text-xl font-bold px-6 pt-10 pb-5'>Who is the most powerful?</h1>
+            <form className='flex-grow px-6'>
+                <label htmlFor="superman" className='input-container p-1 mb-2 md:mb-0 block'>
+                    <input type="radio" name="strongest" id="superman" value='superman' className='mr-2'/>
+                    <span>Superman</span>
+                </label>
+                <label htmlFor="saitama" className='input-container p-1 mb-2 md:mb-0 block'>
+                    <input type="radio" name="strongest" id="saitama" value='saitama' className='mr-2'/>
+                    <span>Saitama</span>
+                </label>
+                <label htmlFor="goku" className='input-container p-1 mb-2 md:mb-0 block'>
+                    <input type="radio" name="strongest" id="goku" value='goku' className='mr-2'/>
+                    <span>Goku</span>
+                </label>
+                <label htmlFor="sinbad" className='input-container p-1 mb-2 md:mb-0 block'>
+                    <input type="radio" name="strongest" id="sinbad" value='sinbad' className='mr-2'/>
+                    <span>Sinbad</span>
+                </label>
+            </form>
+            <button className='bottom-btn p-3 font-semibold'>Submit</button>
         </>
     )
 }
