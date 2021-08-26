3fa982ed2f3a9ead5a4d7f338a19702f46f8942e
amin <amin.m.intichev@gmail.com>
2021-08-26 23:55:44 +0200

    Update height styling to work on mobile

--- components/layout.jsx
+++ components/layout.jsx
@@ -11,7 +11,7 @@ export default function Layout({children}) {
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <meta name="theme-color" content="#892cb3" />
             </Head>
-            <div className='h-screen flex flex-col justify-center items-center'>
+            <div className='main-container flex flex-col justify-center items-center'>
                 <main className='w-11/12 md:w-9/12 lg:max-w-7xl xl:w-7/12 h-4/5 rounded greyish-border shadow bg-white'>
                     {children}
                 </main>
