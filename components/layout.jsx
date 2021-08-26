c7dde9315df70bf6ed87ac6d93e3af2d5c111e01
amin <amin.m.intichev@gmail.com>
2021-08-26 23:56:03 +0200

    Removal theme color meta

--- components/layout.jsx
+++ components/layout.jsx
@@ -9,7 +9,6 @@ export default function Layout({children}) {
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <meta name="theme-color" content="#892cb3" />
             </Head>
             <div className='main-container flex flex-col justify-center items-center'>
                 <main className='w-11/12 md:w-9/12 lg:max-w-7xl xl:w-7/12 h-4/5 rounded greyish-border shadow bg-white'>
